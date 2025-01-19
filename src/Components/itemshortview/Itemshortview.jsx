f5f8f5a1caf0609a36b6df38f4fc4818273e45c7
SriSabarish2021 <srisabarish003@gmail.com>
2025-01-19 12:13:08 +0530

    19-1-2025

--- src/Components/itemshortview/Itemshortview.jsx
+++ src/Components/itemshortview/Itemshortview.jsx
@@ -5,6 +5,7 @@ import { IoMdCheckmarkCircleOutline } from "react-icons/io";
 import { FaHeart } from "react-icons/fa";
 import { FaCaretDown } from "react-icons/fa";
 import { MdOutlineCloseFullscreen } from "react-icons/md";
+import { FaArrowRight } from "react-icons/fa6";
 
 const Itemshortview = ({viewbox,setviewbox}) => {
   return (
@@ -23,7 +24,7 @@ const Itemshortview = ({viewbox,setviewbox}) => {
                 </div>
             </div>
             <div className='item-short-cont-div'>
-                <div className='item-short-cont-head'>
+                {/* <div className='item-short-cont-head'>
                     <div style={{paddingBottom:'10px'}} className='item-short-head-p'>
                         <p className='item-short-name' >Home Useage</p>
                         <div className='star-item-short'>
@@ -44,15 +45,79 @@ const Itemshortview = ({viewbox,setviewbox}) => {
                     </div>
                 </div>
                 <div className='item-short-cont-main'>
-                    <div className='item-short-btn-div'>
-                        <button className='item-short-cartbtn'><div className='item-short-cart-cartbtn-trans'></div> <span className='add-item-short-span'>Add Cart</span></button>
-                        <button  className='item-short-likebtn'><FaHeart className='heart-item-short'/></button>
-                    </div>
+                    
                     <div className='buy-in-item-short-btn'>
                         <button className='buy-btn-item-short'>Buy Item</button>
                     </div>
                     <p className='view-more'>View more info <FaCaretDown className='view-more-svg'/></p>
+                </div> */}
+                <div className='item-shot-head-cont-div'>
+                    <p className='item-shot-heading'>Home Neeeds</p>
+                    <div className='seller-topsell'>
+                        <p className='item-shot-seller'>by Homers</p>
+
+                        <p className='sold-item-initem-short' style={{color:'red'}}><ImFire style={{color:'orangered'}}/>&nbsp; 20 sold on last 36 hours</p>
+                    </div>
                 </div>
+                <div className='item-shot-amt-cont-div'>
+                    <div className='item-short-amt'>
+                        <p className='acutal-amt-in-item-short'>$1000</p>
+                        <p className='dis-amt-in-item-short'>$200</p>
+                    </div>
+                    <div className='item-short-reviews'>
+                        <p className='star-for-item-short'>*****</p>
+                        <p className='review-num-item-short'>20 reviews</p>
+                    </div>
+                    
+                </div>
+                <div className='item-short-available-item'>
+                        <p className='instock'>in stock : Delivery in 5 Days</p>
+                       
+                </div>
+                <div className='description-item-short'>
+                    <p className='descrip-item-short-tit'>Description:</p>
+                    <p className='descrip-item-short-cont'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque tenetur, impedit labore dolores architecto iusto! Enim illum dolor aliquam commodi.</p>
+                    <div className='basic-item-shprt-info'>
+                        <p className='addi-info-short'>Avilability: <span className='addi-span-info-short' style={{color:`rgb(0, 124, 19)`}}><IoMdCheckmarkCircleOutline className='instock-svg'/> inStock</span></p>
+                        <p className='addi-info-short'>Delivery: <span className='addi-span-info-short'>free</span></p>
+                        <p className='addi-info-short'>EMI: <span className='addi-span-info-short'>Available</span></p>
+                    </div>
+                </div>
+                <div className='item-short-btn-div'>
+                        <button className='item-short-cartbtn'><div className='item-short-cart-cartbtn-trans'></div> <span className='add-item-short-span'>Add Cart</span></button>
+                        <button  className='item-short-likebtn'>Add to LikeList</button>
+                </div>
+                <div className='pin-code-eligible-found'>
+                    <p className='pin-eligible'>Eligible for Delivery ?</p>
+                    <div className='pin-inp-div'>
+                        <input className='pin-inp-box' type="number" placeholder='pincode'/>
+                        <div className='enter-pin'>
+                            <FaArrowRight/>
+                        </div>
+                    </div>
+                    <div className='pin-district'>
+                        <p className='pin-dist-p'>Your District : <span className='district-name'>Erode</span></p>
+                        <p className='dist-available-for-delivery'><IoMdCheckmarkCircleOutline className='instock-svg'/>delivery available to this pincode</p>
+                    </div>
+                </div>
+                <div className='buy-now-intem-short-btn'>
+                    <button className='buy-now-btn add-item-short-span'>Buy Now</button>
+                </div>
+                <div className='basic-item-short-info'>
+                    <div className='del-div'>
+                        <p className='logo'>logo</p>
+                        <p className='text'>5 days delivery</p>
+                    </div>
+                    <div className='cash-on-div'>
+                        <p  className='logo'>logo</p>
+                        <p className='text'>cash on deliver</p>
+                    </div>
+                    <div className='return-div'>
+                        <p className='logo'>logo</p>
+                        <p className='text'>easy return</p>
+                    </div>
+                </div>
+                
             </div>
 
 
