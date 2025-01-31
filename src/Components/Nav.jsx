c5eaedda37278af9cae2b53c4f02073685466669
SriSabarish2021 <srisabarish003@gmail.com>
2025-01-31 20:55:20 +0530

    het

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -23,10 +23,12 @@ const Nav = ({navtrue,setlikedisp,arrofcart}) => {
 
 
   return (
-    <div className='navbar' style={{position:navtrue?'fixed':'absolute'}}>
-        <div className='firstnav-cont' style={{backgroundColor:navtrue?` rgba(0, 0, 0, 0.86)`:'transparent',height:navtrue?'65px':'auto'}}>
+   
+    <>
+     <div className='navbar' style={{transform:navtrue?`translateY(-150px)`:`translateY(0px)`,transitionDuration:'1s',position:navtrue?'absolute':'fixed'}}>
+        <div className='firstnav-cont' style={{backgroundColor:'transparent',height:'auto'}}>
             <div className="navfirstdiv">
-                <div className='logo' style={{display:navtrue?'none':'flex'}}>
+                <div className='logo'>
                     <p className="icon"  ><GiFox  className="iconsvg"/></p>
                     <div className="webname">
                         <p>Fox-</p>
@@ -35,11 +37,59 @@ const Nav = ({navtrue,setlikedisp,arrofcart}) => {
                 </div>
                 <div className="locate" >
                     <div className="add">
-                        <p className="address" style={{display:navtrue?'none':'flex'}}>Set ur Address <IoLocation className="locsvg"/></p>
+                        <p className="address">Set ur Address <IoLocation className="locsvg"/></p>
+                        <div className="hoverview"> </div>
+                    </div>
+                   
+                    
+                </div>
+            </div>    
+            <div className="seconddiv">
+                <div className="underlinerel">
+                    <p className={`flx orret hm`}>Home</p>
+                    <div className="lineunder"></div>
+                </div>
+                <div className="underlinerel">
+                    <p className="flx orret">Contact us</p>
+                    <div className="lineunder"></div>
+                </div>
+                <div className="underlinerel">
+                    <p className="flx orret">Order & <span>Returns</span></p>
+                    <div className="lineunder"></div>
+                </div>
+                <div className="underlinerel">
+                    <p className="flx orret">Support</p>
+                    <div className="lineunder"></div>
+                </div>
+
+                <div className="sigupadd">
+                    <div className="flx logdiv">
+                        <p className="login ">Signup <IoIosLogIn className="loginsvg"/></p>
+                    </div>
+                    <div className="signupview"> </div>
+               </div>
+ 
+               
+               
+                
+            </div>
+        </div>
+        
+    </div>
+
+
+
+<div className='navbar'  style={{transform:navtrue?`translateY(0px)`:`translateY(-150px)`,transitionDuration:'1s',position:navtrue?'fixed':'absolute'}}>
+        <div className='firstnav-cont' style={{backgroundColor:` rgba(0, 0, 0, 0.86)`,height:'65px'}}>
+            <div className="navfirstdiv">
+                
+                <div className="locate" >
+                    <div className="add">
+                       
                         <div className="hoverview"> </div>
                     </div>
                     
-                    <div className="serdiv" style={{display:navtrue?'flex':'none'}}>
+                    <div className="serdiv">
                         <input placeholder="Search Prods"  type="search" value={serval} onChange={(e)=>setserval(e.target.value)}/>
                         <button className="serbtn"><FaSearch className="sersvg"/></button>
                     </div>
@@ -60,35 +110,25 @@ const Nav = ({navtrue,setlikedisp,arrofcart}) => {
                     <div className="lineunder"></div>
                 </div>
                 <div className="underlinerel">
-                    {navtrue?(
+                 
                         <Link to='yourcart'>
                                  <p  className="flx cart orret" style={{position:'relative'}}> <GiShoppingCart className="cartsvg" /><span style={{position:'absolute',top:'-10px',color:'orange'}}>{Array.from(arrofcart).length}</span></p>
                         </Link>
-                        ):( <p className="flx orret">Support</p>)}
+                        
                     <div className="lineunder"></div>
                 </div>
 
 
-            {
-                navtrue?(
                     <p onClick={()=>setlikedisp(true)} className="flx like"><FaHeart className="likedsvg">h</FaHeart> Items
                     </p>
-                ):(
-                <div className="sigupadd">
-                    <div className="flx logdiv">
-                        <p className="login ">Signup <IoIosLogIn className="loginsvg"/></p>
-                    </div>
-                    <div className="signupview"> </div>
-               </div>
-              )
-            }
+               
                
                
                
                 
             </div>
         </div>
-        <div className="storedep" style={{display:navtrue?'flex':'none'}}>
+        <div className="storedep">
             <div className="storedep-flx">
                 <p>Departments <FaChevronDown/></p>
             </div>
@@ -112,6 +152,7 @@ const Nav = ({navtrue,setlikedisp,arrofcart}) => {
             </div>
         </div>
     </div>
+    </>
   )
 }
 
