e9b0d56ee9033df72785cb301a0b221ac11139ee
SriSabarish2021 <srisabarish003@gmail.com>
2025-04-15 14:20:46 +0530

    sd

--- src/Components/Cartpage/Cart.jsx
+++ src/Components/Cartpage/Cart.jsx
@@ -112,6 +112,17 @@ let quantitydecre=(id,decquanter)=>{
         setnoitemcheckout(checkout=>!checkout)
         setfooter(true)
     }
+
+
+    const [widthtwo, setWidthtwo] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => setWidthtwo(window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
   return (
     <div className='cart-container' style={{animation:'cartpageanimation 0.4s linear '}}>
         
@@ -125,27 +136,27 @@ let quantitydecre=(id,decquanter)=>{
                 <div  className="underlinerel cont-shoping-hov">
                {String(pathnameforviewmore.pathname).includes(`${id}/yourcart`)?
                 <>
-                    <Link to={`/viewmore/${id}`} className='cont-shopi'><IoArrowBackCircleOutline className="cart-nav-font"/>Back To Item view</Link>
+                    <Link to={`/viewmore/${id}`} className='cont-shopi'><IoArrowBackCircleOutline className="cart-nav-font"/><span style={{display:widthtwo<500?'none':'flex',justifyContent:'center',alignItems:'center'}}>Back To Item view</span></Link>
                     <div className="lineunder" style={{height:'2px'}}></div>
                 </>:String(pathnameforviewmore.pathname).includes(`itemviewall/yourcart`)?
                 <>
-                    <Link to={`/itemviewall`} className='cont-shopi'><IoArrowBackCircleOutline className="cart-nav-font"/>View Items</Link>
+                    <Link to={`/itemviewall`} className='cont-shopi'><IoArrowBackCircleOutline className="cart-nav-font"/><span style={{display:widthtwo<500?'none':'flex',justifyContent:'center',alignItems:'center'}}>View Items</span></Link>
                     <div className="lineunder" style={{height:'2px'}}></div>
                 </>:
                 <>
-                    <Link to='/' className='cont-shopi'><IoArrowBackCircleOutline className="cart-nav-font"/>Continue Shopping</Link>
+                    <Link to='/' style={{display:widthtwo<400?'none':'flex'}} className='cont-shopi'><IoArrowBackCircleOutline className="cart-nav-font"/>Continue Shopping</Link>
                     <div className="lineunder" style={{height:'2px'}}></div>
                 </>}
                     
 
                 </div>
-                <div  className="underlinerel cart-des">
+                <div   className="underlinerel cart-des">
                         
                     <Link  to='/'><RiHome2Line className="cart-nav-font"/></Link>
                     <div className="lineunder"></div>
 
                 </div>
-                <div className="underlinerel cart-des">
+                <div className="underlinerel cart-des contact-no-for-mob">
                          <Link  to='/'><RiContactsLine className="cart-nav-font"/></Link>
                          <div className="lineunder"></div>
 
@@ -164,6 +175,7 @@ let quantitydecre=(id,decquanter)=>{
                     <div className="cart-item-num">
                         <p className="cart-p-1">My Shopping Bag</p>
                         <p className="cart-p-2"><span className="num-item">{Array.from(arrofcart).length} {Array.from(arrofcart).length>1?'items':'item'} </span>in your bag</p>
+                        
                     </div>
                     <div className="cart-list-ofitems">
                         <div className="full-cart">
@@ -172,9 +184,15 @@ let quantitydecre=(id,decquanter)=>{
                                     <p className="cart-hd-p">Product</p>
                                 </div>
                                 <div className="prod-info">
-                                    <p className="cart-hd-p">Price (<LiaRupeeSignSolid/>)</p>
-                                    <p className="cart-hd-p" style={{paddingRight:'15px'}}>Quantity</p>
-                                    <p className="cart-hd-p" style={{paddingRight:'10px'}}>Total Price</p>
+                                    <p className="cart-hd-p">Price&nbsp;<span style={{display:widthtwo<950&&widthtwo>720||widthtwo<670?'none':'flex'}}>(<LiaRupeeSignSolid />)</span></p>
+                                    
+                                    {
+                                        widthtwo<770&&widthtwo>720||widthtwo<670? <p className="cart-hd-p" style={{paddingRight:widthtwo<1000?'0px':'15px'}}>Quant..</p>: <p className="cart-hd-p" style={{paddingRight:widthtwo<1000&&widthtwo>720?'0px':'15px'}}>Quantity</p>
+                                    }
+                                    {
+                                        widthtwo<770&&widthtwo>720||widthtwo<670?<p className="cart-hd-p" style={{paddingRight:'10px'}}>Total</p>:<p className="cart-hd-p" style={{paddingRight:'10px'}}>Total Price</p>
+                                    }
+                                    
                                 </div>
                             </div>
                             <div className="cart-including-item-div">
@@ -185,7 +203,7 @@ let quantitydecre=(id,decquanter)=>{
                                     <div className="product-img-info">
                                         <div className="prod-img" style={{backgroundImage:`url(${indiaddcart.imgurl})`}}></div>
                                         <div className="prod-infor">
-                                            <p className="cart-hd-p-inmain">{indiaddcart.name}</p>
+                                            <p className="cart-hd-p-inmain product-name">{indiaddcart.name}</p>
                                             <p className="cart-hd-p-inmain">{String(indiaddcart.itemdescription).slice(0,30)+'...'}</p>
                                         </div>
                                     </div>
@@ -247,14 +265,14 @@ let quantitydecre=(id,decquanter)=>{
                        ):(<button onClick={()=>noitemsincartalert()} className="chekoutbtn">CheckOut</button>)}
                     <Link to='/'><button className="chekoutbtn">Back To Shop</button></Link>
                 </div>
-                <div className="box">
+                {/* <div className="box">
                     <div className="front"></div>
                     <div className="back"></div>
                     <div className="left"></div>
                     <div className="right"></div>
                     <div className="top"></div>
                     <div className="bottom"></div>
-                </div>
+                </div> */}
             </div>
         </div>
        
